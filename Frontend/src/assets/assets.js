c1e6901d9eb9f6948f13cc8b9836ad31d8682299
Bijay Jha <bijayjha12345@gmail.com>
2026-02-19 17:33:30 +0545

    Images for herocards

--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -1,5 +1,13 @@
 import Logo from './logo.png';
+import HomeCard1 from './dhakaTopi.jpg';
+import HomeCard2 from './dauraSuruwal.jpg';
+import HomeCard3 from './gurungDress.jpg';
+import HomeCard4 from './suitSet.jpg';
 
 export const assets = {
     Logo,
+    HomeCard1,
+    HomeCard2,
+    HomeCard3,
+    HomeCard4,
 }
