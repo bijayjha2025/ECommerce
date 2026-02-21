50421f5d452e57bfb79dca5447cf14726aedaa47
Bijay Jha <bijayjha12345@gmail.com>
2026-02-21 10:09:28 +0545

    Images and data addition for featureSection

---
+++ Frontend/src/Components/FeaturedCollection/FeaturedComponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const FeaturedComponent = () => {
+  return (
+    <div>FeaturedComponent</div>
+  )
+}
+
+export default FeaturedComponent
--- Frontend/src/assets/assets.js
+++ Frontend/src/assets/assets.js
@@ -4,10 +4,33 @@ import HomeCard2 from './dauraSuruwal.jpg';
 import HomeCard3 from './gurungDress.jpg';
 import HomeCard4 from './suitSet.jpg';
 
+// F means featured collection
+import F1 from './dhakaKurta.jpg';
+import F2 from './pashminaWrap.jpg';
+import F3 from './hakkuPatasi.jpg';
+import F4 from './labedaTop.jpg';
+import F5 from './mugaSilk.jpg';
+import F6 from './alloFibre.jpeg';
+import F7 from './bhangraShawl.jpg';
+import F8 from './gunyoCholo.jpg';
+
+
 export const assets = {
     Logo,
     HomeCard1,
     HomeCard2,
     HomeCard3,
     HomeCard4,
+}
+
+export const featuredImages = {
+    F1,
+    F2,
+    F3,
+    F4,
+    F5,
+    F6,
+    F7,
+    F8,
+
 }
